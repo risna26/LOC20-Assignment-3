f45d125658a3234b95650697de48bf75e93b52d3
risna26 <127609009+risna26@users.noreply.github.com>
2023-04-12 00:19:59 +0530

    Add files via upload

---
+++ locQ5.c
@@ -0,0 +1,20 @@
+5. Find the output of the following snippets:
+a. char ****k;
+i) k is a pointer to a pointer to a pointer to a char
+ii) k is a pointer to a pointer to a pointer to a pointer to a char
+iii) k is a pointer to a char pointer
+iv) k is a pointer to a pointer to a char
+ans-- ii
+b. void main()
+{
+int k = 5;
+int *p = &k;
+int **m = &p;
+**m = 6;
+printf("%d\n", k);
+}
+i) 7
+ii) 4
+iii) 6
+iv) Error
+ans-- iii
---
+++ locQ7.c
@@ -0,0 +1,4 @@
+7. What is the size of a C structure? How the size of a C structure is calculated?
+
+ans--Size of a C structure is the sum of sizes of all elements of the structure
+    Size of a structure can be calculated by sizeof() function 
